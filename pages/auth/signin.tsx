fbb4aa5bfe8c564486ed9cc72e7d3bbc554dd2d9
mtergel <tergelm@gmail.com>
2021-01-19 14:59:42 +0800

    meta tags on signin

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,18 +1,49 @@
 import { NextPageContext } from "next";
 import { signIn } from "next-auth/client";
 import { Chakra } from "../../components/wrapper/Chakra";
+import Head from "next/head";
 
 const hostname = process.env.NEXTAUTH_URL || "http://localhost:3001";
 
 export default function SignIn({ cookies }) {
   return (
-    <Chakra cookies={cookies}>
-      <div>
-        <button onClick={() => signIn("spotify", { callbackUrl: hostname })}>
-          Login with spotify
-        </button>
-      </div>
-    </Chakra>
+    <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={`${hostname}/static/apple-touch-icon.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={`${hostname}/static/favicon-32x32.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={`${hostname}/static/favicon-16x16.png`}
+        />
+        <link rel="manifest" href={`${hostname}/static/site.webmanifest`} />
+        <link
+          rel="mask-icon"
+          href={`${hostname}/static/safari-pinned-tab.svg`}
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        <title>Senritsu - Generate your playlist</title>
+      </Head>
+      <Chakra cookies={cookies}>
+        <div>
+          <button onClick={() => signIn("spotify", { callbackUrl: hostname })}>
+            Login with spotify
+          </button>
+        </div>
+      </Chakra>
+    </>
   );
 }
 export async function getServerSideProps(ctx: NextPageContext) {
